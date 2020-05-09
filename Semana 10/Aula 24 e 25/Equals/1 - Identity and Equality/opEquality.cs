f43c1b816fcae33976658eb35739ad90a8253aa7
Nuno Leite <nunocsleite@gmail.com>
2020-05-09 12:10:26 +0100

    Week 10 - Lecture 24-25

---
+++ Semana 10/Aula 24 e 25/Equals/1 - Identity and Equality/opEquality.cs
@@ -0,0 +1,49 @@
+
+using System;
+
+namespace IdentityAndEquality
+{
+
+    // A natureza do tipo (se é Tipo valor ou Tipo referência) influencia o comportamento do código
+    // quando se utiliza o operador ==
+
+    // public struct Point2D
+    public class Point2D
+    {
+        private int x, y;
+
+        public Point2D(int x, int y) { this.x = x; this.y = y; }
+
+        //// Overload do operator==
+        //public static bool operator==(Object o, Point2D p)
+        //{            
+        //     Console.WriteLine("[Point2D] calling operator==");
+        //     return false; // not correctly implemented
+        //}
+        //public static bool operator !=(Object o, Point2D p)
+        //{
+        //    return !(o == p);
+        //}
+    }
+
+    class OpEqualityTest
+    {
+        static void Main1(string[] args)
+        {
+            object o1 = new object();
+            object o2 = new object();
+            bool res1 = o1 == o2; // ceq -> false
+
+            Point2D p1 = new Point2D(1, 2);
+            Point2D p2 = new Point2D(1, 2);
+            // Instrucao IL ceq (compare if equal) => desempilha parametros e compara a igualdade
+
+            Console.WriteLine(p1 == p1); // Se tipo class, e se operator== nao definido. ceq -> true
+                                         // Se operator== definido, invoca operator==
+                                         // Se tipo struct, Erro, se operator== não estiver definido
+            Console.WriteLine(p1 == p2); // Se tipo class, e se operator== nao definido. ceq -> false
+                                         // Se operator== definido, invoca operator==
+                                         // Se tipo struct, Erro, se operator== não estiver definido
+        }
+    }
+}
---
+++ Semana 10/Aula 24 e 25/Equals/3 - Box ValueTypes/V.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Equals.BoxValueTypes
+{
+    // A struct is always sealed
+    public struct V
+    {
+        int x, y;
+
+        // Auxiliary method used in order to avoid boxes
+        public bool Equals(V v) {
+            return x == v.x & y == v.y;
+        }
+
+        public override bool Equals(object o)
+        {
+            if (o is V)
+                return Equals((V)o); // unbox
+            else
+                return false;
+        }
+
+        public override String ToString()
+        {
+            return "Value type V";
+        }
+    }
+
+
+    public class Test {
+        public static void Main1() {
+            V v1 = new V();
+            v1.Equals(v1); // sem box no this nem no parametro => invoca V::Equals(V)
+
+            Console.WriteLine(v1); // box
+            Console.WriteLine(v1.ToString()); // sem box, admitindo que ToString() 
+            // esta redefinido em V
+
+            object o = v1; // box
+            Console.WriteLine(o.ToString()); // invoca BoxedTypeV::ToString() <=> V::ToString()
+
+            Console.WriteLine(o.GetType()); 
+            Console.WriteLine(typeof(V)); 
+            Console.WriteLine(typeof(V) == o.GetType()); // Igual, pois em termos de implementacao
+            // juntaram o Boxed type de V com a tipo V
+            //struct V = .class extends ValueType = Boxed Type V
+
+
+        }
+    }
+}
